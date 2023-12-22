a8644281546415d0c961de9d53be6eebf6e7abad
Parsa Mohammadian <parsa2820@gmail.com>
2023-12-22 17:41:07 +0330

    Optimize fib function

--- ProfilingTest/src/Fibonacci.java
+++ ProfilingTest/src/Fibonacci.java
@@ -1,8 +1,15 @@
+import java.util.HashMap;
+
 public class Fibonacci {
+    private static final HashMap<Integer, Long> mem = new HashMap<Integer, Long>() {{
+       put(0, 1L);
+        put(1, 1L);
+    }};
+
     private static long fib(int n) {
-        if (n == 0 || n == 1)
-            return 1;
-        return fib(n-1) + fib(n-2);
+        if (!mem.containsKey(n))
+            mem.put(n, fib(n-1) + fib(n-2));
+        return mem.get(n);
     }
 
     public static void main(String[] args) {
